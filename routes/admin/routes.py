56857b714496478843fa109518b7e4f87c972499
Ashrafov Rahman <rehman199577@gmail.com>
2021-12-17 21:50:36 +0400

    skills user added

--- routes/admin/routes.py
+++ routes/admin/routes.py
@@ -1,6 +1,8 @@
 from run import app
 from flask import Flask,flash,redirect,url_for,render_template,request
 
+
+
 @app.route("/admin/blog", methods=["GET","POST"])
 def blog():
     from modules import Blogs
@@ -35,3 +37,58 @@ def blog_delete(id):
     db.session.delete(blogs)
     db.session.commit()
     return redirect ("/admin/blog")
+
+
+@app.route("/blogEdit/<int:id>",methods=["GET","POST"])
+def blog_edit(id):
+    from modules import Blogs
+    from run import db
+    newBlogs = Blogs.query.filter_by(id=id).first()
+    if request.method=="POST":
+        blogs = Blogs.query.filter_by(id=id).first()
+        blogs.blog_title = request.form["blog_title"]   
+        blogs.blog_content = request.form["blog_content"]
+        db.session.commit()
+        return redirect("/")
+    return render_template ("/admin/update_blog.html",newBlogs=newBlogs)
+
+
+
+
+@app.route("/admin/home", methods=["GET","POST"])
+def home():
+    from modules import Home
+    import os
+    from run import db
+    from werkzeug.utils import secure_filename
+    homes = Home.query.all()
+    if request.method == "POST":
+        home_content = request.form["home_content"]
+        home = Home(
+            home_content = home_content,
+        )
+        db.session.add(home)
+        db.session.commit()
+        return redirect ("/admin/home")
+    return render_template("admin/home.html", homes=homes)
+
+@app.route("/homeDelete/<int:id>",methods=["GET","POST"])
+def home_delete(id):
+    from modules import Home
+    from run import db
+    homes = Home.query.filter_by(id=id).first()
+    db.session.delete(homes)
+    db.session.commit()
+    return redirect ("/admin/home")
+
+@app.route("/homeUpdate/<int:id>",methods=["GET","POST"])
+def home_edit(id):
+    from modules import Home
+    from run import db
+    newHome = Home.query.filter_by(id=id).first()
+    if request.method=="POST":
+        homes = Home.query.filter_by(id=id).first()  
+        homes.home_coontent = request.form["home_content"]
+        db.session.commit()
+        return redirect("/")
+    return render_template ("/admin/update_home.html",newHome=newHome)
